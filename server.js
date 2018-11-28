3c3ae54880b5496e93fce7558eb04c8e634c891f
Philippe Talbot <philippe_t@live.ca>
2018-11-27 19:14:25 -0500

    created projects page

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects Page',
+		portfolioProjects: 'Projects are here yo'
+	});
+});
+
 app.get('/bad', (req, res) => {
 	res.send({errorMessage: 'Failed son'})
 });
